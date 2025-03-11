be61c19132cb7b6e42fe487d021168b8e050a3f0
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-03-11 07:16:52 +0100

    added new apps

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -49,13 +49,45 @@ INSTALLED_APPS = [
     'django_filters', #Used with DRF
     'rest_framework', #DRF package
     'core', # New app
+    'rest_framework.authtoken',
+    'ecommerce', # New app
 ]
+
+# REST_FRAMEWORK = {
+#     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+#     'DEFAULT_PARSER_CLASSES': (
+#         'rest_framework_json_api.parsers.JSONParser',
+#     ),
+#     'DEFAULT_RENDERER_CLASSES': (
+#         'rest_framework_json_api.renderers.JSONRenderer',
+#         'rest_framework.renderers.BrowsableAPIRenderer'
+#     ),
+#     'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+#     'DEFAULT_FILTER_BACKENDS': (
+#         'rest_framework_json_api.filters.QueryParameterValidationFilter',
+#         'rest_framework_json_api.filters.OrderingFilter',
+#         'rest_framework_json_api.django_filters.DjangoFilterBackend',
+#         'rest_framework.filters.SearchFilter',
+#     ),
+#     'SEARCH_PARAM': 'filter[search]',
+#     'TEST_REQUEST_RENDERER_CLASSES': (
+#         'rest_framework_json_api.renderers.JSONRenderer',
+#     ),
+#     'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json',
+
+#     # 'DEFAULT_PERMISSION_CLASSES': [
+#     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     # ]
+# }
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_json_api.parsers.JSONParser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
@@ -71,11 +103,7 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
     ),
-    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json',
-
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ]
+    'TEST_REQUEST_DEFAULT_FORMAT': 'vnd.api+json'
 }
 
 MIDDLEWARE = [
